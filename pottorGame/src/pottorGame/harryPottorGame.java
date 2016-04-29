a44d76a972ed1de31794726bf2d04192542f462e
Ryan.Ferguson <Ryan.Ferguson@MU-C1-04.mullen.pvt.k12.co.us>
2016-04-29 15:14:02 -0600

    added start of mb test

--- pottorGame/src/pottorGame/harryPottorGame.java
+++ pottorGame/src/pottorGame/harryPottorGame.java
@@ -55,7 +55,14 @@ public class harryPottorGame
 			}
 		public static void buyWand()  throws InterruptedException
 		{
-		int points=0;
+		int I=0;
+		int E=0;
+		int s=0;
+		int w=0;
+		int t=0;
+		int f=0;
+		int p=0;
+		int j=0;
 		System.out.println("As you enter Diagon Ally you notice the giant crooked building ahead, all of the shops along the side.");
 		Thread.sleep(1000);
 		System.out.println("However you know why your here. Your here to get your wand.");
@@ -64,68 +71,62 @@ public class harryPottorGame
 		Thread.sleep(188);
 		System.out.println("he says aaaaa your here for your first wand well lets see it");
 		Thread.sleep(139);
-		System.out.println("do you consider yourself (1) tall (2) medium (3) small");
+		System.out.println("do you prefer (1) action and variety, or (2) quiet time to think things over?");
 		int size = userInput.nextInt();
 		if (size==1)
 			{
-				w.setSize(w.getSize() +12);
-				points = points + 10;
+				E = E +1;
 			}
 		else if (size==2)
 			{
-				w.setSize(w.getSize() +10);
-				points = points +5;
-			}
-		else if (size==3)
-			{
-				w.setSize(w.getSize() +8);
-				points = points + 0;
+				I = I +1;
 			}
 		else
 			{
 				buyWand();
 			}
-		System.out.println("do you consider yourself (1) stubborn, (2) easy going, (3) agressive, (4) passive");
+		System.out.println("do you prefer to think about (1) what is going on now, or (2) the future and what might be?");
 		int attitude = userInput.nextInt();
 		if (attitude==1)
 			{
-				points = points + 15;
+				s = s + 1;
 			}
 		else if (attitude==2)
 			{
-				points = points + 5;
-			}
-		else if (attitude==3)
-			{
-				points = points + 10;
-			}
-		else if (attitude==4)
-			{
-				points = points +0;
+				w = w + 1;
 			}
 		else 
 			{
 				buyWand();
 			}
-		System.out.println("if there was a fork in the road and it you had to choose to go to them (1) mountains, (2) forest, (3) ocian, which would you choose?");
+		System.out.println("Do you prefure to decide (1) on what is best, no matter how you feel, or (2) by how yoou feel about something?");
 		int direction = userInput.nextInt();
 		if (direction==1)
 			{
-				points = points + 10;
+				t = t + 1;
 			}
 		else if (direction==2)
 			{
-				points = points + 5;
-			}
-		else if (direction==3)
-			{
-				points = points + 0;
+				f = f + 1;
 			}
 		else 
 			{
 				buyWand();
 			}
-		System.out.println("what are you most looking forword to studying at hogwarts? (1) Herbology (2) Defence against the Dark arts (3)");
+		System.out.println("Do you like to (1) have a plan before getting started on something, or (2) get going on something by just getting it?");
+		int question4 = userInput.nextInt();
+		if (question4==1)
+			{
+				j = j + 1;
+			}
+		else if (question4==2)
+			{
+				p = p +1;
+			}
+		else 
+			{
+				buyWand();
+			}
 		}
 		
 
